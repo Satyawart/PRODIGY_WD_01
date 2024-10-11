df2720f1ec5255e9a84842d81b8dba8d8d43d674
Satyawart <ssrajawat2205@gmail.com>
2024-10-11 23:05:02 +0530

    Add files via upload

---
+++ reo.js
@@ -0,0 +1,9 @@
+// Change the navbar background when scrolling
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
